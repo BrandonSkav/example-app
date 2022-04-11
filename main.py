dac336ed4cc4792188abdae0df5b429dfa4b53df
Brandon Skavroneck <bskavroneck@smu.edu>
2022-04-11 16:31:45 -0500

    Update

--- main.py
+++ main.py
@@ -1,2 +1,19 @@
+from flask import Flask, jsonify
+from multiprocessing import Value
 import streamlit as st
-clicker = st.button("click")
+
+app = Flask(__name__)
+
+counter = Value('i', 0)
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    clicker = st.button("CLICK")
+    with counter.get_lock():
+        counter.value += 1
+        out = counter.value
+
+    return jsonify(count=out)
+
+app.run()
